322af984f28490007a2616aaab10a4ef2174c860
Elizaveta Kruchinina <milky0105way@gmail.com>
2023-02-07 19:44:47 +0300

    backend v5: изменяет адрес подключения к бд

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const limiter = require('./middlewares/limiter');
 
 const {
   PORT = 3005,
-  MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
 const app = express();
